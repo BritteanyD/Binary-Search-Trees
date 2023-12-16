dea410a924c0500ea07e4dad3a6d44fb7a01dd99
Britteany <miszbritteany@gmail.com>
2023-12-15 19:43:11 -0500

    added tests

---
+++ script.js
@@ -0,0 +1,116 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+  buildTree(arr) {
+    //Sort and remove duplicates here
+    function buildBalancedTree(arr, start, end) {}
+  }
+};
+
+
+let arr = [1, 2, 3];
+let tree = new Tree(arr);
+tree.prettyPrint();
+console.log(tree.isBalanced());
+arr = [57, 8, 33, 55, 29, 7, 72, 9, 71, 20];
+tree = new Tree(arr);
+tree.prettyPrint();
+
+tree.insert(30);
+tree.prettyPrint();
+tree.insert(1);
+tree.prettyPrint();
+tree.insert(23);
+tree.prettyPrint();
+tree.delete(55);
+tree.prettyPrint();
+tree.delete(33);
+tree.prettyPrint();
+tree.delete(29);
+tree.prettyPrint();
+console.log(tree.find(7));
+console.log(tree.find(73));
+console.log(tree.levelOrderIterative());
+tree.levelOrderIterative(({ value }) => {
+  console.log(`value: ${value}`);
+});
+console.log(tree.levelOrderRecursive());
+tree.levelOrderRecursive(({ value }) => {
+  console.log(`value: ${value}`);
+});
+console.log(tree.inorder());
+tree.inorder(({ value }) => {
+  console.log(`value: ${value}`);
+});
+console.log(tree.preorder());
+tree.preorder(({ value }) => {
+  console.log(`value: ${value}`);
+});
+console.log(tree.postorder());
+tree.postorder(({ value }) => {
+  console.log(`value: ${value}`);
+});
+console.log(tree.height());
+console.log(tree.isBalanced());
+tree.rebalance();
+tree.prettyPrint();
+console.log(tree.isBalanced());
+
+// Function to generate an array of random numbers
+function generateRandomArray(size, max) {
+  return Array.from({ length: size }, () => Math.floor(Math.random() * max));
+}
+
+// Driver script
+function driverScript() {
+  // Create a binary search tree from an array of random numbers < 100
+  const randomArray = generateRandomArray(10, 100);
+  const tree = new Tree(randomArray);
+  console.log("Initial Tree:");
+  tree.prettyPrint();
+
+  // Confirm that the tree is balanced
+  console.log("Is the tree balanced? ", tree.isBalanced());
+
+  // Print out all elements in level, pre, post, and in order
+  console.log("Level Order:", tree.levelOrderIterative());
+  console.log("Pre Order:", tree.preorder());
+  console.log("Post Order:", tree.postorder());
+  console.log("In Order:", tree.inorder());
+
+  // Unbalance the tree by adding several numbers > 100
+  tree.insert(101);
+  tree.insert(102);
+  tree.insert(103);
+  console.log("Tree After Adding Elements > 100:");
+  tree.prettyPrint();
+
+  // Confirm that the tree is unbalanced
+  console.log("Is the tree balanced after additions? ", tree.isBalanced());
+
+  // Balance the tree by calling rebalance
+  tree.rebalance();
+  console.log("Tree After Rebalancing:");
+  tree.prettyPrint();
+
+  // Confirm that the tree is balanced
+  console.log("Is the tree balanced after rebalancing? ", tree.isBalanced());
+
+  // Print out all elements in level, pre, post, and in order
+  console.log("Level Order After Rebalancing:", tree.levelOrderIterative());
+  console.log("Pre Order After Rebalancing:", tree.preorder());
+  console.log("Post Order After Rebalancing:", tree.postorder());
+  console.log("In Order After Rebalancing:", tree.inorder());
+}
+
+// Running the driver script
+driverScript();
