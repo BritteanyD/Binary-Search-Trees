f816e0b8109ae39c5173be68192f70e8880d6a66
Britteany <miszbritteany@gmail.com>
2023-12-15 22:03:32 -0500

    inserted nodes

--- script.js
+++ script.js
@@ -11,24 +11,24 @@ class Tree {
     this.root = this.buildTree(arr);
   }
   buildTree = (arr) => {
-    arr.sort(function(a, b) {
-        return a - b;
+    arr.sort(function (a, b) {
+      return a - b;
     });
-    const length = arr.length
-    return this.buildTreeHelper(arr, 0, length - 1)
-  }
+    const length = arr.length;
+    return this.buildTreeHelper(arr, 0, length - 1);
+  };
   buildTreeHelper = (arr, left, right) => {
     //base case
-    if(left > right) {
-        return null;
+    if (left > right) {
+      return null;
     }
     const mid = left + Math.floor((right - left) / 2);
-    const value = arr[mid]
-    const newNode = new Node(value)
-    newNode.left = this.buildTreeHelper(arr, left, mid-1)
-    newNode.right = this.buildTreeHelper(arr, mid + 1, right)
+    const value = arr[mid];
+    const newNode = new Node(value);
+    newNode.left = this.buildTreeHelper(arr, left, mid - 1);
+    newNode.right = this.buildTreeHelper(arr, mid + 1, right);
     return newNode;
-  }
+  };
   isBalanced = () => {
     const res = this.isBalancedHelper();
     return res === -1 ? false : true;
@@ -40,7 +40,7 @@ class Tree {
     }
     const left = this.isBalancedHelper(cur.left),
       right = this.isBalancedHelper(cur.right);
-    if (left === -1 ||  right === -1 ||  Math.abs(left - right) > 1) {
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
       return -1;
     } else {
       return Math.max(this.height(cur.left), this.height(cur.right)) + 1;
@@ -49,44 +49,66 @@ class Tree {
 
   height = (currentNode) => {
     //base case
-    if(currentNode === null) {
-        return 0
+    if (currentNode === null) {
+      return 0;
     }
     const leftHeight = this.height(currentNode.left),
-    rightHeight = this.height(currentNode.right);
+      rightHeight = this.height(currentNode.right);
     return Math.max(leftHeight, rightHeight) + 1;
-  }
+  };
+  insert = (value) => {
+    const newNode = new Node(value);
+    let currentNode = this.root;
+    while (currentNode !== null) {
+      if (currentNode.value < value) {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          break;
+        }
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          break;
+        }
+        currentNode = currentNode.left;
+      }
+    }
+  };
 
   prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
     if (node === null) {
       return;
     }
     if (node.right !== null) {
-      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   };
-};
-
+}
 
 let arr = [3, 2, 1];
 let tree = new Tree(arr);
 tree.prettyPrint();
 console.log(tree.isBalanced());
-arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+arr = [10, 9, 8, 7, 6, 5, 4, 3, 2];
 tree = new Tree(arr);
 tree.prettyPrint();
-/*
+
 tree.insert(30);
 tree.prettyPrint();
 tree.insert(1);
 tree.prettyPrint();
 tree.insert(23);
 tree.prettyPrint();
-tree.delete(55);
+/*tree.delete(55);
 tree.prettyPrint();
 tree.delete(33);
 tree.prettyPrint();
